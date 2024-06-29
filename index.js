61984e8db6be70231ce6904e70721e56834f810c
Volcanus Academy <141003409+volcanusacademy@users.noreply.github.com>
2024-06-29 12:09:40 +0530

    Update index.js with port

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ const userRouter = require('./routes/userRoute');
 app.use(userRouter)
 
 
-app.listen(5000, ()=>{console.log("Server up and running on port 5000!")});
+app.listen(3000, ()=>{console.log("Server up and running on port 3000!")});
